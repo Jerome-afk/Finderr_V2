3016a0a987612fc610f9b8ffbd7eb126973b3750
Jerome-afk <otienojerome7@gmail.com>
2026-04-10 23:42:21 +0300

    Update: Adding a logging package

--- web_server/no_vpn/main.go
+++ web_server/no_vpn/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	utility "finderr_v2/utils"
+	"fmt"
+)
+
 // TODO: Main server implementation
 func main() {
-	
+	// 1. Initialize config
+	cfg, err := utility.InitConfig()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to initialize config: %v", err))
+	}
 }
--- web_server/no_vpn/utils/logger.go
+++ web_server/no_vpn/utils/logger.go
@@ -1,1 +1,26 @@
 package utility
+
+import (
+	"path/filepath"
+	"time"
+)
+
+type LogEntry struct {
+	Level   string `json:"level"`
+	Message string `json:"message"`
+	Time    time.Time  `json:"time"`
+	Data    interface{} `json:"data, omitempty"`
+}
+
+const (
+	logDir = "logs"
+	latestFile = "latest"
+	maxLogSize = 10 * 1024 * 1024 // 10 MB
+	archiveDir = "archive"
+)
+
+var latestFilePath = filepath.Join(logDir, latestFile)
+
+func initLogFolders() error {
+	
+}
