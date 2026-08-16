b09cc5646f441fc0a8f84faf4e479e57faea4a4e
Jerome-afk <otienojerome7@gmail.com>
2026-08-16 17:02:16 +0300

    Database

---
+++ backend/utils/db.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// Ping the database to check if the connection is alive
+func ConnectDB() {
+	godotenv.Load() // Load environment variables from .env file
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+
+	DB = db
+}
