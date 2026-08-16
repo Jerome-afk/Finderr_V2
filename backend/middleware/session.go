fac912d8181261d2f92e97217f4970dfd9ad0402
Jerome-afk <otienojerome7@gmail.com>
2026-08-16 17:34:17 +0300

    Waiting

---
+++ backend/middleware/session.go
@@ -0,0 +1,4 @@
+package middleware
+
+// This file contains middleware functions for the Gin web framework.
+// Main function include handling sessions among other things.
---
+++ backend/utils/logger.go
@@ -0,0 +1,7 @@
+package utils
+
+func Log(message string) {
+	// Here you can implement your logging logic, e.g., write to a file or console
+	// For simplicity, we'll just print to the console for now
+	println(message)
+}
